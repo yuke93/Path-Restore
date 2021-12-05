7b4d813c5bc9f2f43103a51d449f50da2f7387a2
yuke93 <ericyu16@hotmail.com>
2021-12-05 20:59:02 +0800

    clean test

--- codes/dynamic_model_test.py
+++ codes/dynamic_model_test.py
@@ -28,7 +28,6 @@ class DynamicModelTest(DynamicModel):
             is_label = False
         else:
             raise NotImplementedError(test_set)
-        ex_str = 'ex359_random'  # only when is_save=True
 
         # load model
         self.saver_test.restore(self.sess_dyn, load_dir_test)
@@ -38,8 +37,7 @@ class DynamicModelTest(DynamicModel):
         for noi_level in noise_levels:
             # save folder
             if is_save:
-                save_folder_cur = save_folder + \
-                    'noise{}/{}/'.format(noi_level, ex_str)
+                save_folder_cur = save_folder + 'noise{}/'.format(noi_level)
                 if not os.path.exists(save_folder_cur):
                     os.makedirs(save_folder_cur)
 
@@ -48,7 +46,6 @@ class DynamicModelTest(DynamicModel):
 
             # test for each image
             psnr_list = []
-            # ssim_list = []
             count = 0
             total_rb = 0
             total_patches = 0
@@ -59,20 +56,35 @@ class DynamicModelTest(DynamicModel):
                     im_label = test_label[key]
 
                 # run
-                patch_in, pos, count_map = whole2patch(im_data, (data_size, data_size), (stride, stride))
-                feed_dict = {self.input_ph_dyn: patch_in, self.is_train_ph: False}
+                patch_in, pos, count_map = whole2patch(
+                    im_data,
+                    (data_size, data_size),
+                    (stride, stride)
+                )
+                feed_dict = {self.input_ph_dyn: patch_in,
+                             self.is_train_ph: False}
                 patch_len = len(patch_in)
                 if limit_batch < 0 or limit_batch >= patch_in:
-                    patch_out, *actions = self.sess_dyn.run([self.output_dyn] + self.test_info[:-1], feed_dict=feed_dict)
+                    patch_out, *actions = self.sess_dyn.run(
+                        [self.output_dyn] + self.test_info[:-1],
+                        feed_dict=feed_dict
+                    )
                 else:
                     assert 0 < limit_batch < patch_len
                     patch_out = []
                     actions_batch = []
                     # test a batch of patches every time
                     for idx in range(0, patch_len, limit_batch):
-                        patch_cur = patch_in[idx: min(idx + limit_batch, patch_len), ...]
-                        feed_dict = {self.input_ph_dyn: patch_cur, self.is_train_ph: False}
-                        patch_out_cur, *actions_cur = self.sess_dyn.run([self.output_dyn] + self.test_info[:-1], feed_dict=feed_dict)
+                        patch_cur = patch_in[
+                            idx: min(idx + limit_batch, patch_len),
+                            ...
+                        ]
+                        feed_dict = {self.input_ph_dyn: patch_cur,
+                                     self.is_train_ph: False}
+                        patch_out_cur, *actions_cur = self.sess_dyn.run(
+                            [self.output_dyn] + self.test_info[:-1],
+                            feed_dict=feed_dict
+                        )
                         patch_out.append(patch_out_cur)
                         actions_batch.append(actions_cur)
                     patch_out = np.concatenate(patch_out, axis=0)
@@ -80,7 +92,8 @@ class DynamicModelTest(DynamicModel):
                     for act in zip(*actions_batch):
                         act_cur = np.concatenate(act, axis=0)  # num_of_patches
                         actions.append(act_cur)
-                im_output = patch2whole(patch_out, pos, count_map, (stride, stride))
+                im_output = patch2whole(patch_out, pos, count_map,
+                                        (stride, stride))
                 actions = np.array(actions)  # rb_num x num_of_patches
                 actions = np.sum(np.clip(actions, 0., 1.), axis=0)
                 total_rb += np.sum(actions)
@@ -88,7 +101,8 @@ class DynamicModelTest(DynamicModel):
 
                 # save image
                 if is_save:
-                    cv2.imwrite(save_folder_cur + key + '_' + ex_str + '.png', im_output[:, :, ::-1] * 255)
+                    cv2.imwrite(save_folder_cur + key + '.png',
+                                im_output[:, :, ::-1] * 255)
 
                 # PSNR
                 if is_label:
@@ -104,6 +118,7 @@ class DynamicModelTest(DynamicModel):
         avg_rb /= len(noise_levels)
         if is_label:
             avg_psnr /= len(noise_levels)
-            print('average PSNR: {:.3f}, average RB: {:.3f}'.format(avg_psnr, avg_rb))
+            print('average PSNR: {:.3f}, average dynamic \
+                  block: {:.3f}'.format(avg_psnr, avg_rb))
         else:
-            print('average RB: {:.3f}'.format(avg_rb))
+            print('average dynamic block: {:.3f}'.format(avg_rb))
